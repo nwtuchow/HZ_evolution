bce0bdc2d4d4a9cb56e1a9709f066797534478d2
Noah Tuchow <nwtuchow@gmail.com>
2024-05-23 16:48:08 -0400

    Update __init__.py

--- HZ_evolution/__init__.py
+++ HZ_evolution/__init__.py
@@ -1,1 +1,5 @@
-import osROOT_DIR = os.path.dirname(os.path.realpath(__file__))OUTPUT_DIR= ROOT_DIR+'/outputs/'
+import os
+
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_DIR= ROOT_DIR+'/outputs/'
